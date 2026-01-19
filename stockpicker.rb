c01e5a58679f4dd76deccf0e02879a8d8d017d81
guppy <38512219+sourguppyworm@users.noreply.github.com>
2026-01-19 01:12:06 -0500

    Fix inaccuracy bug caused by using endless range

--- stockpicker.rb
+++ stockpicker.rb
@@ -7,8 +7,8 @@ def stock_picker(array)
   array.each_with_index do |buy_price, day|
     # buy = [price, day] if sell[0] != 0 && price < buy[0]
     # p buy
-    array[day...].each_with_index do |sell_price, sell_day|
-      if sell_price - buy_price > profit[0]
+    array[0...array.length()].each_with_index do |sell_price, sell_day|
+      if sell_day > day && sell_price - buy_price > profit[0]
         buy = [buy_price, day]
         sell = [sell_price, sell_day]
         profit[0] = sell_price - buy_price
@@ -16,10 +16,14 @@ def stock_picker(array)
     end
        
   end
-  p "Buy for #{buy[0]} on #{buy[1]}. Sell for #{sell[0]} on #{sell[1]}, for profit of #{profit[0]}}"
+  "Buy for $#{buy[0]} on #{buy[1]}. Sell for $#{sell[0]} on #{sell[1]}, for profit of $#{profit[0]}"
 end
 
 
 
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,1,8,6,1,1])
+p stock_picker([5,7,9,91,8,6,1,5,91])
+p stock_picker([900,7,3,21,8,6,1,5,90])
+p stock_picker([56,700,3,21,8,6,1,5,900])
